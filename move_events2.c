47f87bacdc81feaf6bc43a2dcd60028bf5c5c692
Joe Shaju Perinchery <jperinch@lab1r6s12.42abudhabi.ae>
2024-01-02 11:44:13 +0400

    movement fixed

--- move_events2.c
+++ move_events2.c
@@ -13,19 +13,16 @@ int	move_w(t_data *vars)
 {
 	while (vars->move.w == 1)
 	{
-	if (vars->cx + vars->player.dx * 0.5 < vars->longest_row && vars->cx
-		+ vars->player.dx * 0.5 >= 0 &&(int)(vars->cy
-			+ vars->player.dy * 0.5) < vars->final_c
-		&& (int)(vars->cy
-			+ vars->player.dy * 0.5)>= 0 && (vars->map[(int)(vars->cy
-			+ vars->player.dy * 0.5)][(int)(vars->cx
-			+ vars->player.dx * 0.5)] != 1 && vars->map[(int)(vars->cy
-			+ vars->player.dy * 0.01)][(int)(vars->cx
-			+ vars->player.dx * 0.01)] != 1 ))
-	{	
-        vars->cx += vars->player.dx * (0.1);
-		vars->cy += vars->player.dy * (0.1);
-    }
+	if (vars->cx + vars->player.dx * 0.8 < vars->longest_row && vars->cx
+		+ vars->player.dx * 0.8 >= 0 && vars->cy < vars->final_c
+		&& vars->cy >= 0 && vars->map[(int)vars->cy][(int)(vars->cx
+			+ vars->player.dx * 0.8)] != 1)
+		vars->cx += vars->player.dx * (0.52);
+	if (vars->cx < vars->longest_row && (vars->cx) >= 0 && (int)(vars->cy
+			+ vars->player.dy * 0.8) < vars->final_c && (int)(vars->cy
+			+ vars->player.dy * 0.8) >= 0 && vars->map[(int)(vars->cy
+			+ vars->player.dy * 0.8)][(int)(vars->cx)] != 1)
+		vars->cy += vars->player.dy * (0.52);
 	move(vars);
 	}
 	return (1);
@@ -34,19 +31,16 @@ int	move_s(t_data *vars)
 {
 	while (vars->move.s == 1)
 	{
-	if ((int)vars->cx - vars->player.dx < vars->longest_row && (int)vars->cx - vars->player.dx >= 0
-		&& (int)(vars->cy
-			- vars->player.dy * 0.5) < vars->final_c && (int)(vars->cy
-			- vars->player.dy * 0.5) >= 0 && (vars->map[(int)(vars->cy
-			- vars->player.dy * 0.5)][(int)(vars->cx - vars->player.dx
-				* 0.5)] != 1 && vars->map[(int)(vars->cy
-			- vars->player.dy * 0.01)][(int)(vars->cx
-			- vars->player.dx * 0.01)] != 1 ))
-                {
-                    vars->cx -= vars->player.dx * 0.1;
-                    vars->cy -= vars->player.dy * 0.1;
-
-                }
+	if ((int)vars->cx < vars->longest_row && (int)vars->cx >= 0
+		&& vars->cy < vars->final_c && vars->cy >= 0
+		&& vars->map[(int)vars->cy][(int)(vars->cx - vars->player.dx
+				* 0.8)] != 1)
+		vars->cx -= vars->player.dx * 0.52;
+	if (vars->cx < vars->longest_row && (vars->cx) >= 0 && (int)(vars->cy
+			+ vars->player.dy * 0.8) < vars->final_c && (int)(vars->cy
+			+ vars->player.dy * 0.8) >= 0 && vars->map[(int)(vars->cy
+			- vars->player.dy * 0.8)][(int)(vars->cx)] != 1)
+		vars->cy -= vars->player.dy * 0.52;
 	move(vars);
 	}
 	return (1);
@@ -59,16 +53,16 @@ int	move_a(t_data *vars)
 	{
 	rayAngle = (atan2l(vars->player.dy, vars->player.dx) - vars->directionstart
 			* (PI / 2));
-	if (vars->cx + cos(rayAngle) * 0.5 < vars->longest_row && vars->cx
-		+ cos(rayAngle) * 0.5 >= 0 && (int)(vars->cy
-			+ sin(rayAngle) * 0.5) < vars->final_c && (int)(vars->cy
-			+ sin(rayAngle) * 0.5) >= 0 && (vars->map[(int)(vars->cy+ sin(rayAngle) * 0.5)][(int)(vars->cx + cos(rayAngle)
-			* 0.5)] != 1 && vars->map[(int)(vars->cy+ sin(rayAngle) * 0.01)][(int)(vars->cx + cos(rayAngle)
-			* 0.01)] != 1))
-	{
-		vars->cx += cos(rayAngle) * 0.1;
-		vars->cy += sin(rayAngle) * 0.1;
-	}
+	if (vars->cx + cos(rayAngle) * 0.8 < vars->longest_row && vars->cx
+		+ cos(rayAngle) * 0.8 >= 0 && vars->cy < vars->final_c && vars->cy >= 0
+		&& vars->map[(int)vars->cy][(int)(vars->cx + cos(rayAngle)
+			* 0.8)] != 1)
+		vars->cx += cos(rayAngle) * 0.52;
+	if (vars->cx < vars->longest_row && (vars->cx) >= 0 && (int)(vars->cy
+			+ sin(rayAngle) * 0.8) < vars->final_c && (int)(vars->cy
+			+ sin(rayAngle) * 0.8) >= 0 && vars->map[(int)(vars->cy
+			+ sin(rayAngle) * 0.8)][(int)(vars->cx)] != 1)
+		vars->cy += sin(rayAngle) * 0.52;
 	move(vars);
 	}
 	return (1);
@@ -81,16 +75,17 @@ int	move_d(t_data *vars)
 	{
 	rayAngle = (atan2l(vars->player.dy, vars->player.dx) - vars->directionstart
 			* (PI / 2));
-	if (vars->cx - cos(rayAngle) * 0.5 < vars->longest_row && vars->cx
-		- cos(rayAngle) * 0.5 >= 0 && vars->cy - sin(rayAngle)
-		* 0.5 < vars->final_c && vars->cy - sin(rayAngle) * 0.5 >= 0
-		&& (vars->map[(int)(vars->cy- sin(rayAngle) * 0.5)][(int)(vars->cx - cos(rayAngle)
-			* 0.5)] != 1 && vars->map[(int)(vars->cy- sin(rayAngle) * 0.01)][(int)(vars->cx - cos(rayAngle)
-			* 0.01)] != 1))
-	{
-		vars->cx -= cos(rayAngle) * 0.1;
-		vars->cy -= sin(rayAngle) * 0.1;
-	}
+	if (vars->cx - cos(rayAngle) * 0.8 < vars->longest_row && vars->cx
+		- cos(rayAngle) * 0.8 >= 0 && vars->cy - sin(rayAngle)
+		* 0.8 < vars->final_c && vars->cy - sin(rayAngle) * 0.8 >= 0
+		&& vars->map[(int)(vars->cy)][(int)(vars->cx - cos(rayAngle)
+			* 0.8)] != 1)
+		vars->cx -= cos(rayAngle) * 0.52;
+	if (vars->cx < vars->longest_row && (vars->cx) >= 0 && (int)(vars->cy
+			- sin(rayAngle) * 0.8) < vars->final_c && (int)(vars->cy
+			- sin(rayAngle) * 0.8) >= 0 && vars->map[(int)(vars->cy
+			- sin(rayAngle) * 0.8)][(int)(vars->cx)] != 1)
+		vars->cy -= sin(rayAngle) * 0.52;
 	move(vars);
 	}
 	return (1);
@@ -154,19 +149,27 @@ int	moves(int keycode, t_data *vars)
 	if ((keycode == 13 || keycode == 119) && setmove(&vars->move.w))
 		move_w(vars);
 	else if ((keycode == 1 || keycode == 115) && setmove(&vars->move.s))
+	{
 		move_s(vars);
+	} // a
 	else if ((keycode == 0 || keycode == 97) && setmove(&vars->move.a))
+	{
 		move_a(vars);
+	} // d
 	else if ((keycode == 2 || keycode == 100) && setmove(&vars->move.d))
+	{
 		move_d(vars);
+	}
 	else if ((keycode == 124 || keycode == 65363) && setmove(&vars->move.r))
+	{
 		move_r(vars);
+	} // d
 	else if ((keycode == 123 || keycode == 65361) && setmove(&vars->move.l))
+	{
 		move_l(vars);
+	}
 	if (keycode == 53 || keycode == 65307)
-	    error_free(vars, -1, "Exit\n");
-    if (keycode < 0)
-        exit(0);
+	error_free(vars, -1, "Exit\n");
 	return (0);
 }
 
